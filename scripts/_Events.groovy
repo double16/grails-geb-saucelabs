bb3a4e1e6a5ea0792341da9857aa58a6bb3eef94
Patrick Double <pat@patdouble.com>
2014-11-11 15:18:00 -0600

    Fix logic in useSauceConnect

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,6 +1,8 @@
 
 eventTestPhasesStart = { ->
   def service = Class.forName('grails.plugin.geb.saucelabs.SauceGebTestService').newInstance()
-  service.useSauceConnect = buildConfig.grails.plugin.'geb-saucelabs'.useSauceConnect ?: true
+  if (buildConfig?.grails?.plugin?.'geb-saucelabs'?.useSauceConnect != null) {
+    service.useSauceConnect = buildConfig.grails.plugin.'geb-saucelabs'.useSauceConnect
+  }
   service.init(eventListener)
 }
